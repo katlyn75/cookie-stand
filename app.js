e50207ea1dcdabec3f0aa36e84cabda631df48fb
Katlyn Tucker <studioquirk@yahoo.com>
2017-10-30 18:15:50 -0700

    added to project

--- app.js
+++ app.js
@@ -1,73 +1,67 @@
-"use strict"
+'use strict';
 
-function getRandomIntInclusive(min, max) {
+function getRandomIntInclusive(min, max){
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
-  }
+    return Math.floor(Math.random() * (max - min + 1)) + min;} //The maximum is inclusive and the minimum is inclusive}
 
-const hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm']  
-
+const hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];  
 const pdx = {
-    name: pdxAirport,
+    name:'pdx Airport',
     min: 23,
     max: 65,
     avg: 6.3,
     salesByHour:hourlySales
-}
-    
-
-
+};  
 const pioneer = {
-    name:PioneerPlace,
+    name:'PioneerPlace',
     min:3,
     max:24,
     averagecookies:1.2,
     salesPerHour:hourlySales
-}
+};
 
 const powells = {
-    name:Powells,
+    name:'Powells',
     min:11,
     max:38,
     averagecookies:3.7,
     salesPerHour:hourlySales
-}
+};
 
-const stjohns = {
-    name:StJohns,
+const stJohns = {
+    name:'StJohns',
     min:20,
     max:38,
     averagecookies:2.3,
     salesPerHour:hourlySales
-}
+};
 
 const waterfront = {
-    name:Waterfront,
+    name:'Waterfront',
     min:2,
     max:16,
     averagecookies:4.6,
     salesPerHour:hourlySales
-}
+};
 
 function hourlySales(){
     const salesPerHour = [];
-    for (let i = 0; i <hours.length; i++){
-        const hour= hours[i];
+    for (let i = 0; i < hours.length; i++){
+        const hour = hours[i];
         const numCustomers = getRandomIntInclusive(this.min, this.max);
         const numCookies = numCustomers * this.avg;
         const oneHour = {
             hour: hour,
             cookiesSold: numCookies
-        }
-        salesByHour.push(oneHour);
+        };
+        salesPerHour.push(oneHour);
     }
-    this.salesByHour = salesByHour;
+    this.salesPerHour = salesPerHour;
 }
-
-
-
 pdx.hourlySales();
+pioneer.hourlySales();
+powells.hourlySales();
+stJohns.hourlySales();
+waterfront.hourlySales();
 console.log(pdx);
-
-//calcSales: function (){};
