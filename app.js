6d29fcced598bcc52aa0ebc435cd52310ab0af4c
Katlyn Tucker <studioquirk@yahoo.com>
2017-11-01 15:55:53 -0700

    built constructor function

--- app.js
+++ app.js
@@ -8,61 +8,28 @@ function getRandomIntInclusive(min, max){
 
 //store hours   
 const hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
-function render (){}
-//store object literals
-const pdx = {
-    name:'pdx Airport',
-    min: 23,
-    max: 65,
-    averageCookies: 6.3,
-    cookiesSold:hourlySales,
-    salesPerHour:[],
-    render:render
-};
-const pioneer = {
-    name:'PioneerPlace',
-    min:3,
-    max:24,
-    averageCookies:1.2,
-    cookiesSold:hourlySales,
-    salesPerHour:[],
-    render:render
-};
 
-const powells = {
-    name:'Powells',
-    min:11,
-    max:38,
-    averageCookies:3.7,
-    cookiesSold:hourlySales,
-    salesPerHour:[],
-    render:render
-};
-const stJohns = {
-    name:'StJohns',
-    min:20,
-    max:38,
-    averageCookies:2.3,
-    cookiesSold:hourlySales,
-    salesPerHour:[],
-    render:render
-};
-const waterfront = {
-    name:'Waterfront',
-    min:2,
-    max:16,
-    averageCookies:4.6,
-    cookiesSold:hourlySales,
-    salesPerHour:[],
-    render:render
-};
+//constructor function for object literals
+const pdx = new Store('pdx Airport',23,65,6.3);
+const pioneer = new Store('Pioneer Place',3,24,1.2);
+const powells = new Store('Powell\s',11,38,3.7);
+const stJohns = new Store('St John\s',20,38,2.3);
+const waterfront = new Store('Waterfront',2,16,4.6);
 
-function hourlySales(){
+function Store(name, min, max, averageCookies){
+    this.name = name;
+    this.min = min;
+    this.max = max;
+    this.ave = averageCookies;
+};
+//calls protoype of store and hourly sales of cookies
+Store.prototype.hourlySales=function(){
     const salesPerHour = [];
     for (let i = 0; i < hours.length; i++){
         const hour = hours[i];
         const numCustomers = getRandomIntInclusive(this.min, this.max);
-        const numCookies = Math.floor(numCustomers * this.averageCookies);
+        const numCookies = Math.floor(numCustomers * this.ave);
+        console.log(numCookies);
         const oneHour = {
             hour: hour,
             cookiesSold: numCookies
@@ -70,13 +37,15 @@ function hourlySales(){
         salesPerHour.push(oneHour);
     }
     this.salesPerHour = salesPerHour;
+    
 }
-pdx.cookiesSold();
-pioneer.cookiesSold();
-powells.cookiesSold();
-stJohns.cookiesSold();
-waterfront.cookiesSold();
-console.log(pdx);
+
+pdx.hourlySales();
+pioneer.hourlySales();
+powells.hourlySales();
+stJohns.hourlySales();
+waterfront.hourlySales();
+
 
 
 
