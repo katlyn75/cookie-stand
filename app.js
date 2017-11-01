fa9406733c4c1b4983d03b2b3139539f2deeb4b2
Katlyn Tucker <studioquirk@yahoo.com>
2017-11-01 14:32:01 -0700

    tested total cookies

--- app.js
+++ app.js
@@ -3,31 +3,30 @@
 function getRandomIntInclusive(min, max){
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;} //The maximum is inclusive and the minimum is inclusive}
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}; //The maximum is inclusive and the minimum is inclusive}
 
 //store hours   
 const hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
-
+function render (){}
 //store object literals
 const pdx = {
     name:'pdx Airport',
     min: 23,
     max: 65,
     averageCookies: 6.3,
-    cookiesSold:hourlySales
+    cookiesSold:hourlySales,
     salesPerHour:[],
     render:render
-    
 };
 const pioneer = {
     name:'PioneerPlace',
     min:3,
     max:24,
     averageCookies:1.2,
-    cookiesSold:hourlySales
+    cookiesSold:hourlySales,
     salesPerHour:[],
     render:render
-    
 };
 
 const powells = {
@@ -35,27 +34,25 @@ const powells = {
     min:11,
     max:38,
     averageCookies:3.7,
-    cookiesSold:hourlySales
+    cookiesSold:hourlySales,
     salesPerHour:[],
     render:render
 };
-
 const stJohns = {
     name:'StJohns',
     min:20,
     max:38,
     averageCookies:2.3,
-    cookiesSold:hourlySales
+    cookiesSold:hourlySales,
     salesPerHour:[],
     render:render
 };
-
 const waterfront = {
     name:'Waterfront',
     min:2,
     max:16,
     averageCookies:4.6,
-    cookiesSold:hourlySales
+    cookiesSold:hourlySales,
     salesPerHour:[],
     render:render
 };
@@ -80,17 +77,6 @@ powells.cookiesSold();
 stJohns.cookiesSold();
 waterfront.cookiesSold();
 console.log(pdx);
-
-//function render (){
-//  const div = document.getElementById ('SalmonCookies');
-//const h2 = createElement
-
-//    const totalSales = [];
-//    for(let i = 0; i <stores..length; i ++);{
-//    stores = [i];
-//}
-
-
 
 
 
